0e70ee7928f3b34b8c11d93541bb7e66ddfce8d3
Mubeen Mehmood <mubeendev3@gmail.com>
2023-12-26 02:13:56 +0500

    Project 2: Number Guessing Game Completed

---
+++ 02-NumberGuessingGameProject/index.ts
@@ -0,0 +1,122 @@
+#!/usr/bin/env node
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+import inquirer from "inquirer";
+
+class NumberGuessingGame {
+  private targetNumber: number;
+  constructor() {
+    // Initialize the target number randomly using Math library
+    this.targetNumber = Math.floor(Math.random() * 10) + 1;
+  }
+
+  // This method is used to stop the animations or add delay
+  stopAnimation(animation: any, duration: number): Promise<void> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        animation.stop();
+        resolve();
+      }, duration * 1000);
+    });
+  }
+  // This method is used to display starting of our game
+  async greeting() {
+    const animation = chalkAnimation.rainbow(`
+Guess the secret number and conquer the game!
+     ___________________________________
+    | _____ |   | ___ | ___ ___ | |   | |
+    | |   | |_| |__ | |_| __|____ | | | |
+    | | | |_________|__ |______ |___|_| |
+    | |_|   | _______ |______ |   | ____|
+    | ___ | |____ | |x_____ | |_| |____ |
+    |___|_|____ | |   ___ | |________ | |
+    |   ________| | |__ | |______ | | | |
+    | | | ________| | __|____ | | | __| |
+    |_| |__ |   | __|__ | ____| | |_| __|
+    |   ____| | |____ | |__ |   |__ |__ |
+    | |_______|_______|___|___|___|_____|
+
+    ______           __     __)                   
+  (, /    )        (, /|  /|       /)            
+    /---(            / | / |      (/_  _   _ __  
+ ) / ____) (_/_   ) /  |/  |_(_(_/_) _(/__(/_/ (_ ... <3
+(_/ (     .-/    (_/   '                         
+         (_/                                     
+
+
+        `);
+    await this.stopAnimation(animation, 2);
+    this.gameLogic();
+  }
+  // This method is used to restart the game
+  async restartGame() {
+    const restartGame = await inquirer.prompt([
+      {
+        name: "userDecision",
+        type: "list",
+        message: chalk.magenta.bold.underline(
+          "\nFeeling lucky for another try?"
+        ),
+        choices: ["Yes", "No"],
+      },
+    ]);
+    console.log("\n");
+    if (restartGame.userDecision === "Yes") {
+      this.gameLogic();
+    } else {
+      const endingAnimation = chalkAnimation.rainbow(
+        `Good Bye! See you next time... :)`
+      );
+      await this.stopAnimation(endingAnimation, 3);
+    }
+  }
+  // This is the main game logic
+  async gameLogic() {
+    let userLife: number = 5;
+    while (true) {
+      console.log(chalk.bgRed(` ${userLife} Lives Remaining! `));
+
+      // Getting input from the user
+      const userInput = await inquirer.prompt([
+        {
+          name: "userNumber",
+          type: "number",
+          message: chalk.green.underline.bold(
+            "\nTime to guess! What's the number?"
+          ),
+        },
+      ]);
+
+      // This variable is used to get the difference between the target value and the user guess
+      const differece: number = Math.abs(
+        this.targetNumber - userInput.userNumber
+      );
+      if (userLife !== 1) {
+        if (differece === 0) {
+          console.log(
+            chalk.bgMagenta.bold(
+              " Congratulations! You guessed the correct number! "
+            )
+          );
+          break;
+        } else if (differece <= 2) {
+          console.log(chalk.yellow("You're too close! Keep going!"));
+          userLife--;
+        } else {
+          userLife--;
+          console.log(chalk.yellow("You're far away. Try again!"));
+        }
+      } else {
+        console.log(chalk.bgRed(`Awww! Game Over`));
+        break;
+      }
+    }
+    this.restartGame();
+  }
+  async main() {
+    this.greeting();
+  }
+}
+// Creating an instance of the Class
+const game = new NumberGuessingGame();
+game.main();
---
+++ 02-NumberGuessingGameProject/readme.md
@@ -0,0 +1,26 @@
+# Number Guessing Game CLI
+
+Welcome to the Number Guessing Game CLI, a fun and interactive command-line game built with Node.js, Chalk, and Inquirer.
+
+## Features
+
+- 🌈 Rainbow-themed CLI interface powered by Chalk
+- 🔢 Guess the secret number and conquer the game
+- 🚀 Engaging animations for a delightful experience
+
+## Installation & Usage
+
+1. Install the calculator globally: `npm install -g mubeen-numguessing`
+2. Run the calculator: `npx mubeen-numguessing`
+3. Follow the prompts to play game with style!
+
+## How to Play
+
+- You have 5 lives to guess the correct number.
+- The game provides feedback on your guess, indicating if you are close or far away.
+- If you guess the correct number, congratulations! The game will prompt if you want to play again.
+- If you run out of lives, the game ends, and you can choose to play again.
+
+## Issues
+
+If you encounter any issues or have suggestions, please message me on the [GitHub](https://github.com/mubeendev3).
