c4a322077075925d34a13a64eecf64c272be0c11
Mubeen Mehmood <mubeendev3@gmail.com>
2024-01-03 03:05:42 +0500

    ATM Machine Project Completed

--- 03-ATM-MachineProject/index.js
+++ 03-ATM-MachineProject/index.js
@@ -22,7 +22,8 @@ class ATMMachine {
     |  [ 1 ] Withdraw  |
     |  [ 2 ] Deposit   |
     |  [ 3 ] Balance   |
-    |  [ 4 ] Exit      |
+    |  [ 4 ] Transfer  |
+    |  [ 5 ] Exit      |
     '------------------'
 `, {
             title: "ATM Machine Project",
@@ -31,7 +32,7 @@ class ATMMachine {
             padding: 1,
             borderColor: "magenta",
         }));
-        await this.stopAnimations(startingAnimation, 0);
+        await this.stopAnimations(startingAnimation, 3);
         this.askUserDetails();
     }
     // Method to prompt the user for their details using Inquirer
@@ -67,9 +68,10 @@ class ATMMachine {
                 },
             },
         ]);
-        this.afterUserLoggedIn(userDetails.userName);
+        await this.afterUserLoggedIn(userDetails.userName, userDetails.userPin);
+        this.restartProcess();
     }
-    async depositMoney(currentBalance, userName) {
+    async withdrawMoney(currentBalance, userName) {
         const userInput = await inquirer.prompt([
             {
                 type: "input",
@@ -120,11 +122,185 @@ class ATMMachine {
             }));
         }
     }
-    async askUserOperations(userBalance, userName) {
+    async depositMoney(currentBalance, userName) {
+        const userInput = await inquirer.prompt([
+            {
+                type: "input",
+                name: "amount",
+                message: chalk.green("Please Enter The Amount You Want To Deposit: $"),
+            },
+            {
+                name: "receipt",
+                type: "input",
+                message: chalk.green("Do You Want To Generate Receipt? Type Y or N:"),
+                validate: function (input) {
+                    const userInput = input;
+                    if (userInput.toLowerCase() === "y" ||
+                        userInput.toLowerCase() === "n") {
+                        return true;
+                    }
+                    else {
+                        chalk.red.bold(`Please Enter A Valid Letter 'Y' or 'N'`);
+                    }
+                },
+            },
+        ]);
+        if (userInput.receipt.toLowerCase() === "y") {
+            const depositAmount = Number.parseInt(userInput.amount);
+            const totalBalance = currentBalance + depositAmount;
+            console.log(boxen(chalk.hex("#FF00FF")("Name: ") +
+                chalk.green.bold(userName) +
+                "\n" +
+                chalk.hex("#FF00FF")("Deposit Amount: ") +
+                chalk.green.bold(`$${depositAmount}`) +
+                "\n" +
+                chalk.hex("#FF00FF")("Total Balance: ") +
+                chalk.green.bold(`$${totalBalance}`), {
+                title: "Receipt",
+                borderColor: "magenta",
+                borderStyle: "double",
+                padding: 1,
+                margin: 1,
+            }));
+        }
+    }
+    checkAccountInformation(currentBalance, userName) {
+        console.log(boxen(chalk.hex("#FF00FF")("Name: ") +
+            chalk.green.bold(userName) +
+            "\n" +
+            chalk.hex("#FF00FF")("Account ID: ") +
+            chalk.green.bold("0x9243...") +
+            "\n" +
+            chalk.hex("#FF00FF")("Gender: ") +
+            chalk.green.bold("Male") +
+            "\n" +
+            chalk.hex("#FF00FF")("Account Type: ") +
+            chalk.green.bold("Current") +
+            "\n" +
+            chalk.hex("#FF00FF")("Total Balance: ") +
+            chalk.green.bold(`$${currentBalance}`), {
+            title: "Account Information",
+            borderColor: "magenta",
+            borderStyle: "double",
+            padding: 1,
+            margin: 1,
+        }));
+    }
+    async transferBalance(currentUserPin, currentBalance, userName) {
+        const userInput = await inquirer.prompt([
+            {
+                type: "input",
+                name: "recipientAccount",
+                message: chalk.green("Enter Recipient's Account Number: (e.g., 0x2345...)"),
+            },
+            {
+                type: "password",
+                name: "userPin",
+                message: "Enter Your 4-Digits Pin Code:",
+                mask: "*",
+                validate: function (input) {
+                    // Validate that the PIN is a 4-digit number
+                    if (input === currentUserPin) {
+                        return true;
+                    }
+                    else {
+                        return `The PIN you provided doesn't match your previous PIN.`;
+                    }
+                },
+            },
+            {
+                type: "input",
+                name: "amount",
+                message: chalk.green("Please Enter amount you want to transfer: $"),
+                validate: function (input) {
+                    const amount = Number.parseInt(input);
+                    if (amount !== 0 && amount <= currentBalance) {
+                        return true;
+                    }
+                    else {
+                        return chalk.red.bold("Insufficient funds. Please enter a valid withdrawal amount.");
+                    }
+                },
+            },
+            {
+                name: "receipt",
+                type: "input",
+                message: chalk.green("Do You Want To Generate Receipt? Type Y or N:"),
+                validate: function (input) {
+                    const userInput = input;
+                    if (userInput.toLowerCase() === "y" ||
+                        userInput.toLowerCase() === "n") {
+                        return true;
+                    }
+                    else {
+                        chalk.red.bold(`Please Enter A Valid Letter 'Y' or 'N'`);
+                    }
+                },
+            },
+        ]);
+        if (userInput.receipt.toLowerCase() === "y") {
+            const transferAmount = userInput.amount;
+            const remainingBalance = currentBalance - userInput.amount;
+            console.log(boxen(chalk.hex("#FF00FF")("Name: ") +
+                chalk.green.bold(userName) +
+                "\n" +
+                chalk.hex("#FF00FF")("Transfered To: ") +
+                chalk.green.bold(userInput.recipientAccount) +
+                "\n" +
+                chalk.hex("#FF00FF")("Amount Transferred: ") +
+                chalk.green.bold(`$${transferAmount}`) +
+                "\n" +
+                chalk.hex("#FF00FF")("Status: ") +
+                chalk.green.bold(`Successful`) +
+                "\n" +
+                chalk.hex("#FF00FF")("Remaining Balance: ") +
+                chalk.green.bold(`$${remainingBalance}`), {
+                title: "Receipt",
+                borderColor: "magenta",
+                borderStyle: "double",
+                padding: 1,
+                margin: 1,
+            }));
+        }
+    }
+    async restartProcess() {
+        const userDecision = await inquirer.prompt([
+            {
+                name: "decision",
+                type: "input",
+                message: chalk.green("Do You Want To Start Again? Type Y or N:"),
+                // validate: function (input) {
+                //   if (input.toLowerCase() === "y" || input.toLowerCase() === "n") {
+                //     return true;
+                //   } else {
+                //     chalk.red.bold(`Please Enter A Valid Letter 'Y' or 'N'`);
+                //   }
+                // },
+            },
+        ]);
+        if (userDecision.decision.toLowerCase() === "y") {
+            this.main();
+        }
+        else {
+            this.endingAnimation();
+        }
+    }
+    async endingAnimation() {
+        const endingAnimation = chalkAnimation.neon(boxen(`Thank You For Using Our ATM MACHINE!`, {
+            title: "Developed By Mubeen Mehmood",
+            titleAlignment: "center",
+            borderStyle: "arrow",
+            padding: 0.5,
+            borderColor: "magenta",
+        }));
+        await this.stopAnimations(endingAnimation, 3);
+    }
+    async askUserOperations(userBalance, userName, currentUserPin) {
         const atmOperations = [
-            "Deposit Money",
             "Withdraw Money",
-            "Check Balance",
+            "Deposit Money",
+            "Account Information",
+            "Transfer Balance",
             "Exit",
         ];
         const userOperation = await inquirer.prompt([
@@ -137,19 +313,24 @@ class ATMMachine {
         ]);
         switch (userOperation.operations) {
             case atmOperations[0]:
-                this.depositMoney(userBalance, userName);
+                await this.withdrawMoney(userBalance, userName);
                 break;
             case atmOperations[1]:
+                await this.depositMoney(userBalance, userName);
                 break;
             case atmOperations[2]:
+                this.checkAccountInformation(userBalance, userName);
                 break;
             case atmOperations[3]:
+                await this.transferBalance(currentUserPin, userBalance, userName);
+                break;
+            case atmOperations[4]:
                 break;
             default:
                 break;
         }
     }
-    afterUserLoggedIn(userName) {
+    async afterUserLoggedIn(userName, userPin) {
         const userBalance = this.randomBalanceGenerator();
         clear();
         console.log(boxen(chalk.hex("#FF00FF")("Name: ") +
@@ -162,15 +343,19 @@ class ATMMachine {
             borderStyle: "double",
             padding: 1,
             margin: 1,
-            //   textAlignment: "center",
+            titleAlignment: "center",
         }));
-        this.askUserOperations(userBalance, userName);
+        await this.askUserOperations(userBalance, userName, userPin);
     }
     randomBalanceGenerator() {
         const randomBalance = Math.floor(Math.random() * 100000);
         return randomBalance;
     }
+    main() {
+        clear();
+        this.welcomeScreen();
+    }
 }
 // Creating an instance of the ATMMachine class and running the welcome screen
 const myATM = new ATMMachine();
-myATM.welcomeScreen();
+myATM.main();
--- 03-ATM-MachineProject/index.ts
+++ 03-ATM-MachineProject/index.ts
@@ -26,7 +26,8 @@ class ATMMachine {
     |  [ 1 ] Withdraw  |
     |  [ 2 ] Deposit   |
     |  [ 3 ] Balance   |
-    |  [ 4 ] Exit      |
+    |  [ 4 ] Transfer  |
+    |  [ 5 ] Exit      |
     '------------------'
 `,
         {
@@ -38,7 +39,7 @@ class ATMMachine {
         }
       )
     );
-    await this.stopAnimations(startingAnimation, 0);
+    await this.stopAnimations(startingAnimation, 3);
     this.askUserDetails();
   }
 
@@ -73,9 +74,10 @@ class ATMMachine {
         },
       },
     ]);
-    this.afterUserLoggedIn(userDetails.userName);
+    await this.afterUserLoggedIn(userDetails.userName, userDetails.userPin);
+    this.restartProcess();
   }
-  async depositMoney(currentBalance: number, userName: string) {
+  async withdrawMoney(currentBalance: number, userName: string) {
     const userInput = await inquirer.prompt([
       {
         type: "input",
@@ -134,11 +136,215 @@ class ATMMachine {
       );
     }
   }
-  async askUserOperations(userBalance: number, userName: string) {
+  async depositMoney(currentBalance: number, userName: string) {
+    const userInput = await inquirer.prompt([
+      {
+        type: "input",
+        name: "amount",
+        message: chalk.green("Please Enter The Amount You Want To Deposit: $"),
+      },
+      {
+        name: "receipt",
+        type: "input",
+        message: chalk.green("Do You Want To Generate Receipt? Type Y or N:"),
+        validate: function (input) {
+          const userInput: string = input;
+          if (
+            userInput.toLowerCase() === "y" ||
+            userInput.toLowerCase() === "n"
+          ) {
+            return true;
+          } else {
+            chalk.red.bold(`Please Enter A Valid Letter 'Y' or 'N'`);
+          }
+        },
+      },
+    ]);
+    if (userInput.receipt.toLowerCase() === "y") {
+      const depositAmount: number = Number.parseInt(userInput.amount);
+      const totalBalance: number = currentBalance + depositAmount;
+      console.log(
+        boxen(
+          chalk.hex("#FF00FF")("Name: ") +
+            chalk.green.bold(userName) +
+            "\n" +
+            chalk.hex("#FF00FF")("Deposit Amount: ") +
+            chalk.green.bold(`$${depositAmount}`) +
+            "\n" +
+            chalk.hex("#FF00FF")("Total Balance: ") +
+            chalk.green.bold(`$${totalBalance}`),
+          {
+            title: "Receipt",
+            borderColor: "magenta",
+            borderStyle: "double",
+            padding: 1,
+            margin: 1,
+          }
+        )
+      );
+    }
+  }
+  checkAccountInformation(currentBalance: number, userName: string) {
+    console.log(
+      boxen(
+        chalk.hex("#FF00FF")("Name: ") +
+          chalk.green.bold(userName) +
+          "\n" +
+          chalk.hex("#FF00FF")("Account ID: ") +
+          chalk.green.bold("0x9243...") +
+          "\n" +
+          chalk.hex("#FF00FF")("Gender: ") +
+          chalk.green.bold("Male") +
+          "\n" +
+          chalk.hex("#FF00FF")("Account Type: ") +
+          chalk.green.bold("Current") +
+          "\n" +
+          chalk.hex("#FF00FF")("Total Balance: ") +
+          chalk.green.bold(`$${currentBalance}`),
+        {
+          title: "Account Information",
+          borderColor: "magenta",
+          borderStyle: "double",
+          padding: 1,
+          margin: 1,
+        }
+      )
+    );
+  }
+  async transferBalance(
+    currentUserPin: number,
+    currentBalance: number,
+    userName: string
+  ) {
+    const userInput = await inquirer.prompt([
+      {
+        type: "input",
+        name: "recipientAccount",
+        message: chalk.green(
+          "Enter Recipient's Account Number: (e.g., 0x2345...)"
+        ),
+      },
+      {
+        type: "password",
+        name: "userPin",
+        message: "Enter Your 4-Digits Pin Code:",
+        mask: "*",
+        validate: function (input) {
+          // Validate that the PIN is a 4-digit number
+          if (input === currentUserPin) {
+            return true;
+          } else {
+            return `The PIN you provided doesn't match your previous PIN.`;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "amount",
+        message: chalk.green("Please Enter amount you want to transfer: $"),
+
+        validate: function (input) {
+          const amount: number = Number.parseInt(input);
+          if (amount !== 0 && amount <= currentBalance) {
+            return true;
+          } else {
+            return chalk.red.bold(
+              "Insufficient funds. Please enter a valid withdrawal amount."
+            );
+          }
+        },
+      },
+      {
+        name: "receipt",
+        type: "input",
+        message: chalk.green("Do You Want To Generate Receipt? Type Y or N:"),
+        validate: function (input) {
+          const userInput: string = input;
+          if (
+            userInput.toLowerCase() === "y" ||
+            userInput.toLowerCase() === "n"
+          ) {
+            return true;
+          } else {
+            chalk.red.bold(`Please Enter A Valid Letter 'Y' or 'N'`);
+          }
+        },
+      },
+    ]);
+    if (userInput.receipt.toLowerCase() === "y") {
+      const transferAmount: number = userInput.amount;
+      const remainingBalance: number = currentBalance - userInput.amount;
+      console.log(
+        boxen(
+          chalk.hex("#FF00FF")("Name: ") +
+            chalk.green.bold(userName) +
+            "\n" +
+            chalk.hex("#FF00FF")("Transfered To: ") +
+            chalk.green.bold(userInput.recipientAccount) +
+            "\n" +
+            chalk.hex("#FF00FF")("Amount Transferred: ") +
+            chalk.green.bold(`$${transferAmount}`) +
+            "\n" +
+            chalk.hex("#FF00FF")("Status: ") +
+            chalk.green.bold(`Successful`) +
+            "\n" +
+            chalk.hex("#FF00FF")("Remaining Balance: ") +
+            chalk.green.bold(`$${remainingBalance}`),
+
+          {
+            title: "Receipt",
+            borderColor: "magenta",
+            borderStyle: "double",
+            padding: 1,
+            margin: 1,
+          }
+        )
+      );
+    }
+  }
+  async restartProcess(): Promise<void> {
+    const userDecision = await inquirer.prompt([
+      {
+        name: "decision",
+        type: "input",
+        message: chalk.green("Do You Want To Start Again? Type Y or N:"),
+        // validate: function (input) {
+        //   if (input.toLowerCase() === "y" || input.toLowerCase() === "n") {
+        //     return true;
+        //   } else {
+        //     chalk.red.bold(`Please Enter A Valid Letter 'Y' or 'N'`);
+        //   }
+        // },
+      },
+    ]);
+    if (userDecision.decision.toLowerCase() === "y") {
+      this.main();
+    } else {
+      this.endingAnimation();
+    }
+  }
+  async endingAnimation(): Promise<void> {
+    const endingAnimation = chalkAnimation.neon(
+      boxen(`Thank You For Using Our ATM MACHINE!`, {
+        title: "Developed By Mubeen Mehmood",
+        titleAlignment: "center",
+        borderStyle: "arrow",
+        padding: 0.5,
+        borderColor: "magenta",
+      })
+    );
+    await this.stopAnimations(endingAnimation, 3);
+  }
+  async askUserOperations(
+    userBalance: number,
+    userName: string,
+    currentUserPin: number
+  ) {
     const atmOperations: string[] = [
-      "Deposit Money",
       "Withdraw Money",
-      "Check Balance",
+      "Deposit Money",
+      "Account Information",
+      "Transfer Balance",
       "Exit",
     ];
     const userOperation = await inquirer.prompt([
@@ -151,13 +357,18 @@ class ATMMachine {
     ]);
     switch (userOperation.operations) {
       case atmOperations[0]:
-        this.depositMoney(userBalance, userName);
+        await this.withdrawMoney(userBalance, userName);
         break;
       case atmOperations[1]:
+        await this.depositMoney(userBalance, userName);
         break;
       case atmOperations[2]:
+        this.checkAccountInformation(userBalance, userName);
         break;
       case atmOperations[3]:
+        await this.transferBalance(currentUserPin, userBalance, userName);
+        break;
+      case atmOperations[4]:
         break;
 
       default:
@@ -165,7 +376,7 @@ class ATMMachine {
     }
   }
 
-  afterUserLoggedIn(userName: string) {
+  async afterUserLoggedIn(userName: string, userPin: number): Promise<void> {
     const userBalance: number = this.randomBalanceGenerator();
     clear();
     console.log(
@@ -181,18 +392,22 @@ class ATMMachine {
           borderStyle: "double",
           padding: 1,
           margin: 1,
-          //   textAlignment: "center",
+          titleAlignment: "center",
         }
       )
     );
-    this.askUserOperations(userBalance, userName);
+    await this.askUserOperations(userBalance, userName, userPin);
   }
   randomBalanceGenerator(): number {
     const randomBalance: number = Math.floor(Math.random() * 100000);
     return randomBalance;
   }
+  main() {
+    clear();
+    this.welcomeScreen();
+  }
 }
 
 // Creating an instance of the ATMMachine class and running the welcome screen
 const myATM = new ATMMachine();
-myATM.welcomeScreen();
+myATM.main();
