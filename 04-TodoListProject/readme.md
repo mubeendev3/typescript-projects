6baae15aaf461b23e8bd5dbf70b0e3f5d1617c08
Mubeen Mehmood <mubeendev3@gmail.com>
2024-01-24 23:14:02 +0500

    🌟 TODO App Project Completed! 🚀

---
+++ 04-TodoListProject/readme.md
@@ -0,0 +1,42 @@
+# TODO App CLI
+
+Welcome to the TODO App CLI, a command-line interface for managing your tasks. This project is built with TypeScript, Inquirer, Boxen, and Chalk Animation, providing a user-friendly experience for handling TODO operations.
+
+## Features
+
+- 🌈 Colorful CLI interface powered by Chalk Animation
+- 🔄 Smooth animations for an interactive feel
+- 📝 Add, view, update, and delete tasks in your TODO list
+- 🚀 Simple and intuitive user input handling with Inquirer
+
+## Dependencies
+
+- [inquirer](https://www.npmjs.com/package/inquirer)
+- [boxen](https://www.npmjs.com/package/boxen)
+- [chalk-animation](https://www.npmjs.com/package/chalk-animation)
+
+## Installation & Usage
+
+1. Install the TODO App CLI globally: `npm install -g mubeen-todoproject`
+2. Run the TODO App CLI: `npx mubeen-todoproject`
+3. Follow the prompts to perform various TODO operations.
+
+## How to Use
+
+1. Choose from options like Add, View, Update, Delete, or Exit.
+2. Follow the prompts to complete the selected operation.
+3. View and manage your TODO list with ease.
+
+## Example
+
+```bash
+npx mubeen-todoproject
+```
+
+## Follow Me:
+
+🌐 Connect with Mubeen on [LinkedIn](https://www.linkedin.com/in/mubeendeveloper/)<br>
+🐙 Explore Mubeen's projects on [GitHub](https://github.com/mubeendev3)<br>
+📸 Follow Mubeen's creative journey on [Instagram](https://www.instagram.com/mubeendeveloper/)<br>
+🐦 Stay updated with Mubeen on [Twitter](https://twitter.com/mubeendeveloper)<br>
+🎨 Discover Mubeen's design portfolio on [Behance](https://www.behance.net/pixuro)<br>
---
+++ 04-TodoListProject/todo.js
@@ -0,0 +1,127 @@
+#!/usr/bin/env node
+import boxen from "boxen";
+import chalkAnimation from "chalk-animation";
+import inquirer from "inquirer";
+class TodoApp {
+    todoItemsList = [];
+    async sleep() {
+        await new Promise((resolve) => {
+            setTimeout(resolve, 3000);
+        });
+    }
+    async welcomeScreen() {
+        const startingAnimation = chalkAnimation.karaoke(boxen(`
+  Welcome to our ⤹★ TODO App ★⤸  
+            
+      .------------------.
+      |  [ 1 ] Add       |
+      |  [ 2 ] View      |
+      |  [ 3 ] Update    |
+      |  [ 4 ] Delete    |
+      |  [ 5 ] Exit      |
+      '------------------'
+`, {
+            title: "TODO App Project",
+            titleAlignment: "center",
+            borderStyle: "classic",
+            float: "left",
+            //   borderColor: "magenta",
+        }));
+        await this.sleep();
+        startingAnimation.stop();
+        this.main();
+    }
+    async endingScreen() {
+        const endingAnimation = chalkAnimation.karaoke(`Thank You For Using Our Todo App!`);
+        await this.sleep();
+        endingAnimation.stop();
+    }
+    async addTask() {
+        const userInput = await inquirer.prompt([
+            {
+                type: "input",
+                name: "todoItem",
+                message: "Add item in your TODO List: ",
+            },
+        ]);
+        this.todoItemsList.push(userInput.todoItem);
+        console.log("Task Added Successfully!");
+    }
+    async deleteTask() {
+        const userInput = await inquirer.prompt([
+            {
+                type: "list",
+                name: "todoItem",
+                message: "Which Task You Wanted To Delete?",
+                choices: this.todoItemsList,
+            },
+        ]);
+        const deletedTaskIndex = this.todoItemsList.indexOf(userInput.todoItem);
+        this.todoItemsList.splice(deletedTaskIndex, 1);
+        console.log(`Task ${userInput.todoItem} Deleted Successfully!`);
+    }
+    async updateTask() {
+        const userInput = await inquirer.prompt([
+            {
+                type: "list",
+                name: "todoItem",
+                message: "Choose Task You Wanted To Update",
+                choices: this.todoItemsList,
+            },
+            {
+                type: "input",
+                name: "newTodoItem",
+                message: "Enter Updated Task: ",
+            },
+        ]);
+        const oldTaskIndex = this.todoItemsList.indexOf(userInput.todoItem);
+        this.todoItemsList[oldTaskIndex] = userInput.newTodoItem;
+        console.log(`Task Updated Successfully!`);
+    }
+    async main() {
+        let breakLoop = true;
+        const todoOperations = [
+            "Add",
+            "View",
+            "Update",
+            "Delete",
+            "Exit",
+        ];
+        do {
+            const userInput = await inquirer.prompt([
+                {
+                    name: "choice",
+                    type: "list",
+                    message: "Which operation you want to perform?",
+                    choices: todoOperations,
+                },
+            ]);
+            switch (userInput.choice) {
+                case todoOperations[0]:
+                    await this.addTask();
+                    break;
+                case todoOperations[1]:
+                    console.log(this.todoItemsList);
+                    break;
+                case todoOperations[2]:
+                    await this.updateTask();
+                    break;
+                case todoOperations[3]:
+                    if (this.todoItemsList.length === 0) {
+                        console.log(`Oops Nothing To Delete Add Something First`);
+                        continue;
+                    }
+                    else {
+                        await this.deleteTask();
+                    }
+                    break;
+                case todoOperations[4]:
+                    await this.endingScreen();
+                    breakLoop = false;
+                    break;
+            }
+        } while (breakLoop);
+    }
+}
+const todoApp = new TodoApp();
+todoApp.welcomeScreen();
