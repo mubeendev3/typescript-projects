4c58707c0998288f9853fc217afd35e1daef0423
Mubeen Mehmood <mubeendev3@gmail.com>
2024-01-26 16:21:12 +0500

    🌟 Word Counter Project Completed! 🚀

---
+++ 06-WordCounterProject/index.js
@@ -0,0 +1,86 @@
+#!/usr/bin/env node
+// Importing necessary libraries for user input, text formatting, and animations
+import inquirer from "inquirer";
+import boxen from "boxen";
+import chalkAnimation from "chalk-animation";
+// WordCounter class to handle the word counting application
+class WordCounter {
+    // Method to stop animations after a specified duration
+    stopAnimations(animation, duration) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                animation.stop();
+                resolve();
+            }, duration * 1000);
+        });
+    }
+    // Method to display the welcome screen with an animated box
+    async welcomeScreen() {
+        // Creating a neon animated box with a welcome message
+        const startingAnimation = chalkAnimation.neon(boxen(`
+       ┓ ┏     ┓  ┏┓           
+       ┃┃┃┏┓┏┓┏┫  ┃ ┏┓┓┏┏┓╋┏┓┏┓
+       ┗┻┛┗┛┛ ┗┻  ┗┛┗┛┗┻┛┗┗┗ ┛        
+        `, {
+            title: "Word Counter Project",
+            titleAlignment: "center",
+            borderStyle: "classic",
+            borderColor: "magenta",
+        }));
+        // Stopping the animation after 3 seconds and moving to the next step
+        await this.stopAnimations(startingAnimation, 3);
+        await this.getUserInput();
+    }
+    // Method for ending animation and thanking the user
+    async endingAnimation() {
+        // Creating a neon animated box with a thank you message
+        const endingAnimation = chalkAnimation.neon(boxen(`Thank You For Using Our WORD COUNTER!`, {
+            title: "Developed By Mubeen Mehmood",
+            titleAlignment: "center",
+            borderStyle: "classic",
+            padding: 0.5,
+            borderColor: "magenta",
+        }));
+        // Stopping the animation after 3 seconds
+        await this.stopAnimations(endingAnimation, 3);
+    }
+    // Private method to get user input for the paragraph
+    async getUserInput() {
+        const userInput = await inquirer.prompt([
+            {
+                name: "userParagraph",
+                type: "input",
+                message: "Enter your paragraph: ",
+            },
+        ]);
+        // Counting words based on user input
+        this.countWords(userInput.userParagraph);
+    }
+    // Private method to count words and ask the user if they want to try again
+    async countWords(userParagraph) {
+        // Choices for the user to try again or end the program
+        const userChoices = ["Yes", "No"];
+        let paragraphList = userParagraph.split(" ");
+        let wordCounter = paragraphList.length;
+        console.log(`Total Words Are: ${wordCounter}`);
+        // Asking the user if they want to try again
+        const userInput = await inquirer.prompt([
+            {
+                type: "list",
+                name: "userDecision",
+                message: "Do you want to try again?",
+                choices: userChoices,
+            },
+        ]);
+        // If the user chooses to try again, get user input again; otherwise, end the program
+        if (userInput.userDecision === userChoices[0]) {
+            await this.getUserInput();
+        }
+        else {
+            await this.endingAnimation();
+        }
+    }
+}
+// Creating an instance of the WordCounter class and starting the word counter application
+const myWordCounter = new WordCounter();
+myWordCounter.welcomeScreen();
---
+++ 06-WordCounterProject/readme.md
@@ -0,0 +1,48 @@
+# Word Counter CLI
+
+Welcome to the Word Counter CLI, a command-line interface for counting words in a given paragraph. This project is built with TypeScript, Inquirer, Chalk Animation, and Boxen, providing an engaging experience for users to count words interactively.
+
+## Features
+
+- 🌈 Colorful CLI interface powered by Chalk Animation
+- 🔄 Smooth animations for an interactive feel
+- 📝 Real-time word counting based on user input
+- 🚀 Simple and intuitive user input handling with Inquirer
+
+## Dependencies
+
+- [inquirer](https://www.npmjs.com/package/inquirer)
+- [chalk-animation](https://www.npmjs.com/package/chalk-animation)
+- [boxen](https://www.npmjs.com/package/boxen)
+
+## Installation & Usage
+
+1. Clone the repository: `git clone https://github.com/mubeendev3/typescript-projects.git`
+2. Change into the project directory: `cd 06-WordCounterProject`
+3. Install dependencies: `npm install`
+4. Run the Word Counter CLI: `npx mubeen-wordcounter`
+5. Follow the prompts to enter your paragraph and count words.
+
+## How to Use
+
+1. Enter the paragraph you want to analyze.
+2. View the total word count for the given paragraph.
+3. Choose whether to try counting words again or exit the program.
+
+## Example
+
+```bash
+npx mubeen-wordcounter
+```
+
+## Issues
+
+If you encounter any issues or have suggestions, please report them on the [GitHub repository](https://github.com/mubeendev3/typescript-projects/issues).
+
+## Follow Me:
+
+🌐 Connect with Mubeen on [LinkedIn](https://www.linkedin.com/in/mubeendeveloper/)<br>
+🐙 Explore Mubeen's projects on [GitHub](https://github.com/mubeendev3)<br>
+📸 Follow Mubeen's creative journey on [Instagram](https://www.instagram.com/mubeendeveloper/)<br>
+🐦 Stay updated with Mubeen on [Twitter](https://twitter.com/mubeendeveloper)<br>
+🎨 Discover Mubeen's design portfolio on [Behance](https://www.behance.net/pixuro)<br>
