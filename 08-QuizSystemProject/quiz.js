8942ba4dc159489730d0bccaf9f3383e32af8d82
Mubeen Mehmood <mubeendev3@gmail.com>
2024-01-30 17:37:12 +0500

    🌟 Quiz App Project Completed! 🚀

---
+++ 08-QuizSystemProject/quiz.js
@@ -0,0 +1,166 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+import boxen from "boxen";
+class QuizApp {
+    userScore = 0;
+    questions = [
+        {
+            question: "What does TypeScript bring to JavaScript?",
+            options: [
+                "Static Typing",
+                "Dynamic Typing",
+                "No Typing",
+                "Strong Typing",
+            ],
+            correctAnswer: "Static Typing",
+        },
+        {
+            question: "Which keyword is used to declare a variable with a fixed type in TypeScript?",
+            options: ["let", "var", "const", "type"],
+            correctAnswer: "const",
+        },
+        {
+            question: 'What is the purpose of the "interface" keyword in TypeScript?',
+            options: [
+                "To define a class",
+                "To define a function",
+                "To define a type structure",
+                "To create an object",
+            ],
+            correctAnswer: "To define a type structure",
+        },
+        {
+            question: 'What is the TypeScript equivalent of "undefined" in JavaScript?',
+            options: ["null", "undefined", "void", "NaN"],
+            correctAnswer: "undefined",
+        },
+        {
+            question: "How do you define an optional property in a TypeScript interface?",
+            options: [
+                "property?",
+                "property!: type",
+                "property?: type",
+                "property!: type | undefined",
+            ],
+            correctAnswer: "property?: type",
+        },
+        {
+            question: "Which TypeScript compiler option is used to watch for file changes and recompile?",
+            options: ["--outFile", "--watch", "--strict", "--target"],
+            correctAnswer: "--watch",
+        },
+        {
+            question: "What is a union type in TypeScript?",
+            options: [
+                "A type that can be either null or undefined",
+                "A type that can be of multiple types",
+                "A type that can only be null",
+                "A type that can only be undefined",
+            ],
+            correctAnswer: "A type that can be of multiple types",
+        },
+        {
+            question: "How do you explicitly specify the type of a variable in TypeScript?",
+            options: [
+                'Using the "type" keyword',
+                'Using the "var" keyword',
+                'Using the "as" keyword',
+                'Using the "let" keyword',
+            ],
+            correctAnswer: 'Using the "as" keyword',
+        },
+        {
+            question: "Which version of ECMAScript is TypeScript based on?",
+            options: ["ES5", "ES6", "ES2015", "ES2019"],
+            correctAnswer: "ES2015",
+        },
+        {
+            question: 'What is the purpose of the "readonly" modifier in TypeScript?',
+            options: [
+                "To make a property writable",
+                "To make a property read-only",
+                "To specify a variable type",
+                "To define a constant",
+            ],
+            correctAnswer: "To make a property read-only",
+        },
+    ];
+    totalScore = this.questions.length;
+    async welcomeScreenAnimation() {
+        const animation = chalkAnimation.rainbow(boxen(`
+ Welcome To Our
+┏┓┳┳┳┏┓  ┏┓┏┓┏┓
+┃┃┃┃┃┏┛  ┣┫┃┃┃┃
+┗┻┗┛┻┗┛  ┛┗┣┛┣┛
+Project
+       `, {
+            title: "Quiz App Project",
+            titleAlignment: "center",
+            textAlignment: "center",
+            margin: 1,
+            padding: 1,
+            borderStyle: "double",
+        }));
+        // Stop the animation after a specified duration
+        await this.stopAnimation(animation, 3);
+        this.startQuiz();
+    }
+    // Method for ending animation and thanking the user
+    async endingAnimation() {
+        const endingAnimation = chalkAnimation.rainbow(`Thank You For Using Our QUIZ APP!`);
+        await this.stopAnimation(endingAnimation, 3);
+    }
+    // Stop animations after a specified duration
+    stopAnimation(animation, duration) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                animation.stop();
+                resolve();
+            }, duration * 1000);
+        });
+    }
+    async startQuiz() {
+        for (const question of this.questions) {
+            await this.askQuestion(question);
+        }
+        console.log(chalk.bgCyan.bold(` Your Scored: ${this.userScore}/${this.totalScore}! \n`));
+        await this.restartQuiz();
+    }
+    async askQuestion(question) {
+        const answer = await inquirer.prompt({
+            type: "list",
+            name: "userAnswer",
+            message: chalk.green.bold.underline(question.question),
+            choices: question.options,
+        });
+        this.checkAnswer(answer.userAnswer, question.correctAnswer);
+    }
+    checkAnswer(userAnswer, correctAnswer) {
+        if (userAnswer === correctAnswer) {
+            this.userScore++;
+        }
+        console.log();
+    }
+    async restartQuiz() {
+        const userChoices = ["Yes", "No"];
+        const userInput = await inquirer.prompt([
+            {
+                type: "list",
+                name: "userDecision",
+                message: "Do you want to start quiz again?",
+                choices: userChoices,
+            },
+        ]);
+        if (userInput.userDecision === userChoices[0]) {
+            this.startQuiz();
+        }
+        else {
+            this.endingAnimation();
+        }
+    }
+}
+// Create an instance of the QuizApp class
+const quizApp = new QuizApp();
+// Start the quiz
+quizApp.welcomeScreenAnimation();
---
+++ 08-QuizSystemProject/readme.md
@@ -0,0 +1,41 @@
+# TypeScript Quiz App
+
+Welcome to the TypeScript Quiz App, a command-line interface (CLI) quiz game built with TypeScript, Inquirer, Chalk, Chalk Animation, and Boxen. Test your knowledge of TypeScript with this interactive quiz!
+
+## Features
+
+- 🚀 Multiple-choice quiz questions.
+- 🎨 Colorful and interactive console interface.
+- 🌈 Welcome and ending animations.
+- 📈 Score tracking and restart option.
+
+## Dependencies
+
+- [Inquirer](https://www.npmjs.com/package/inquirer)
+- [Chalk](https://www.npmjs.com/package/chalk)
+- [Chalk Animation](https://www.npmjs.com/package/chalk-animation)
+- [Boxen](https://www.npmjs.com/package/boxen)
+
+## Installation & Usage
+
+1. Install the ATM CLI globally: `npm install -g mubeen-quizproject`
+2. Run the ATM CLI: `npx mubeen-quizproject`
+3. Follow the prompts to perform various ATM operations.
+
+## How to Use
+
+1. Answer each quiz question by choosing from the provided options.
+2. Your score will be displayed at the end of the quiz.
+3. Optionally, restart the quiz to challenge yourself again.
+
+## Issues
+
+If you encounter any issues or have suggestions, please report them on the [GitHub repository](https://github.com/mubeendev3/typescript-projects/issues).
+
+## Follow Me:
+
+🌐 Connect with Mubeen on [LinkedIn](https://www.linkedin.com/in/mubeendeveloper/)<br>
+🐙 Explore Mubeen's projects on [GitHub](https://github.com/mubeendev3)<br>
+📸 Follow Mubeen's creative journey on [Instagram](https://www.instagram.com/mubeendeveloper/)<br>
+🐦 Stay updated with Mubeen on [Twitter](https://twitter.com/mubeendeveloper)<br>
+🎨 Discover Mubeen's design portfolio on [Behance](https://www.behance.net/pixuro)<br>
